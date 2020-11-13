cdf350766f9b4438abdbd4fd9fb749311a1af1e0
mayermakes <49558118+mayermakes@users.noreply.github.com>
2020-11-13 12:56:34 +0100

    Add files via upload

---
+++ DL.py
@@ -0,0 +1,9 @@
+#yt-downloader
+import os
+
+file = open("list.txt", "r")
+for line in file:
+  print("downloading video from url: " + line)
+  os.system("youtube-dl + line )
+print("finished downloading")
+
